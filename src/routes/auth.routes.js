8a046bb1090336b3ce8b9edcc2bdad5e69f9963f
ankush-sihag <ankushsihag@gmail.com>
2026-05-19 15:50:18 +0530

    validations

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,19 +7,26 @@ const {
 
 const { protect } = require('../middleware/auth.middleware');
 
+const { registerValidator, loginValidator } = require('../validators/auth.validator');
+
+const validate = require('../validators/validationResult.validator');
+
 const router = express.Router();
+// console.log('1. registerValidator is:', typeof registerValidator);
+// console.log('2. validate is:', typeof validate);
+// console.log('3. registerUser is:', typeof registerUser);
 
-router.post('/register', registerUser);
+router.post('/register', registerValidator, validate, registerUser);
 
-router.post('/login', loginUser);
+router.post('/login', loginValidator, validate, loginUser);
 
-router.get('/me', protect, (req, res) => {
+// router.get('/me', protect, (req, res) => {
 
-    res.status(200).json({
-        success: true,
-        user: req.user
-    });
+//     res.status(200).json({
+//         success: true,
+//         user: req.user
+//     });
 
-});
+// });
 
 module.exports = router;
---
+++ src/validators/auth.validator.js
@@ -0,0 +1,37 @@
+const { body } = require('express-validator');
+
+const registerValidator = [
+    body('name')
+        .notEmpty()
+        .withMessage('Name is required')
+        .isLength({ min: 3 })
+        .withMessage('Name must be atleast 3 characters'),
+
+    body('email')
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Invalid email format'),
+
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 6 })
+        .withMessage('Password must be atleast 6 characters'),
+
+];
+
+const loginValidator = [
+    body('email')
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Invalid email'),
+
+    body('password')
+        .notEmpty()
+        .withMessage('password is required'),
+
+];
+
+module.exports = { registerValidator, loginValidator };
